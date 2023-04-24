ade158ca9a1184145dff9f44a0be8eb12d9802a2
Lidietholivar98 <lolivar40908@ufide.ac.cr>
2023-04-23 22:13:05 -0600

    Se integran los cambios hechos por Leonardo y se arreglan algunos defectos encontrados

--- ProyectoFinal/DispensadorPastillas/DispensadorPastillas.ino
+++ ProyectoFinal/DispensadorPastillas/DispensadorPastillas.ino
@@ -1,17 +1,18 @@
 #include <Servo.h>
 #include <Adafruit_NeoPixel.h>
-#include <Adafruit_LiquidCrystal.h>
+//#include <Adafruit_LiquidCrystal.h>
 #include <EEPROM.h>
+#include <LiquidCrystal.h>
 
-#define NUMPIXELS 9 // número de neopixeles en strip
-#define LUZ_1_PIN 4  // Pin de luz 1
-#define LUZ_2_PIN 10 // Pin de luz 2
-#define LUZ_3_PIN 9  // Pin de luz 3
-#define LUZ_4_PIN 7  // Pin de luz 4
-#define SERVO_2_PIN 2 // Pin del servo 2
-#define SERVO_3_PIN 3 // Pin del servo 3
-#define SERVO_4_PIN 5 // Pin del servo 4
-#define SERVO_5_PIN 6 // Pin del servo 5
+#define NUMPIXELS 9    // número de neopixeles en strip
+#define LUZ1_PIN 4     // Pin de luz 1
+#define LUZ2_PIN 10    // Pin de luz 2
+#define LUZ3_PIN 9     // Pin de luz 3
+#define LUZ4_PIN 7     // Pin de luz 4
+#define SERVO_2_PIN 2  // Pin del servo 2
+#define SERVO_3_PIN 3  // Pin del servo 3
+#define SERVO_4_PIN 5  // Pin del servo 4
+#define SERVO_5_PIN 6  // Pin del servo 5
 // Límites de tiempo
 #define HOURS_IN_DAY 24
 #define MINUTES_IN_HOUR 60
@@ -24,55 +25,60 @@
 //BUZZER
 #define PIN_BUZZ 8
 
-int hora = 12;
-int minuto = 0;
-int segundo = 0;
-int am_pm = 1, am_pm_alarma = 0; //am: 0; pm: 1
+//lcd
+#define RS 14
+#define E 15
+#define D4 16
+#define D5 17
+#define D6 19
+#define D7 18
+
+int hora = 12, minuto = 0, segundo = 0;
+int am_pm = 1, am_pm_alarma = 0;    //am: 0; pm: 1
+int am_pm2 = 1, am_pm_alarma2 = 0;  //am: 0; pm: 1
+int am_pm3 = 1, am_pm_alarma3 = 0;  //am: 0; pm: 1
 
 //Estado de los botones
-int estado1;
-int estado2;
-int estado3;
+int estado1, estado2, estado3;
+int horaAlarma = 0;
+int minutoAlarma = 0;
+int segundoAlarma = 0;
+int horaAlarma2 = 0;
+int segundoAlarma2 = 0;
+int minutoAlarma2 = 0;
+int horaAlarma3 = 0;
+int minutoAlarma3 = 0;
+int segundoAlarma3 = 0;
 
-int horaAlarma=0, minutoAlarma=0, segundoAlarma=0;
-bool esAlarma = false;//variable para controlar ciclo configuracion alarma
-bool esConfigHora = false;//variable para controlar ciclo configuracion hora
-bool sonarAlarma = false;//variable para controlar alarma
+bool esAlarma = false;      //variable para controlar ciclo configuracion alarma
+bool esAlarma2 = false;     //variable para controlar ciclo configuracion alarma
+bool esAlarma3 = false;     //variable para controlar ciclo configuracion alarma
+bool esConfigHora = false;  //variable para controlar ciclo configuracion hora
+bool sonarAlarma = false;   //variable para controlar alarma
 
 int sonido = 250;  // Frecuencia buzzer
 
-// Alarma 1
-int alarm_hour_1 = 10;
-int alarm_minute_1 = 23;
-int alarm_second_1 = 6;
-
-// Alarma 2
-int alarm_hour_2 = 10;
-int alarm_minute_2 = 25;
-
 // variables repetitivas
-byte counter = 0;
-byte red_color = 0;
-byte green_color = 0;
-byte blue_color = 0;
+byte contador = 0, redColor = 0, greenColor = 0, blueColor = 0;
 
 // Definición de variables y objetos
-Adafruit_LiquidCrystal lcd(0);
+//Adafruit_LiquidCrystal lcd(0);
+LiquidCrystal lcd(RS, E, D4, D5, D6, D7);
 Servo servo_2;
 Servo servo_3;
 Servo servo_4;
 Servo servo_5;
-Adafruit_NeoPixel luz_1(NUMPIXELS, LUZ_1_PIN, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel luz_2(NUMPIXELS, LUZ_2_PIN, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel luz_3(NUMPIXELS, LUZ_3_PIN, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel luz_4(NUMPIXELS, LUZ_4_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel luz1(NUMPIXELS, LUZ1_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel luz2(NUMPIXELS, LUZ2_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel luz3(NUMPIXELS, LUZ3_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel luz4(NUMPIXELS, LUZ4_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-  luz_1.begin();
-  luz_2.begin();
-  luz_3.begin();
-  luz_4.begin();
   lcd.begin(16, 2);
+  luz1.begin();
+  luz2.begin();
+  luz3.begin();
+  luz4.begin();
 
   // Configuración del pin de los servomotores
   servo_2.attach(SERVO_2_PIN);
@@ -96,16 +102,199 @@ void setup() {
 }
 
 void loop() {
+  //configuracion de alarmas
+
+  //alarma 1
+  while (hora == horaAlarma && minuto == minutoAlarma) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Hora de pastilla");
+    delay(500);
+    lcd.setCursor(0, 1);
+    lcd.print("DISPENSADOR 1Y4");
+    delay(1000);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("  Clonazepan Y");
+    delay(500);
+    lcd.setCursor(0, 1);
+    lcd.print("  Quetiapina  ");
+    delay(500);
+
+    setColor();
+
+    for (int i = 0; i < NUMPIXELS; i++) {
+      luz1.setPixelColor(i, luz1.Color(redColor, greenColor, blueColor));
+      luz4.setPixelColor(i, luz4.Color(redColor, greenColor, blueColor));
+      luz1.show();
+      luz4.show();
+
+      delay(200);
+    }
+
+    // Starting the motor that opens the lid
+    servo_2.write(180);
+    servo_5.write(180);
+    delay(1000);
+    servo_5.write(0);
+    servo_2.write(0);
+
+    delay(1000);
+
+    // reproducir sonido
+    tone(8, 440);
+    delay(500);
+    noTone(8);
+    delay(100);
+    // apagar luces servo y limpiar pantalla
+    lcd.clear();
+    contador++;
+    segundo++;
+    if (contador == 3) {
+      horaAlarma++;
+      for (int i = 0; i < NUMPIXELS; i++) {
+        luz1.setPixelColor(i, 0, 0, 0);
+        luz4.setPixelColor(i, 0, 0, 0);
+        luz1.show();
+        luz4.show();
+        delay(200);
+        contador = 0;
+      }
+    }
+  }
+
+  //ALARMA 2
+  while (hora == horaAlarma2 && minuto == minutoAlarma2) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Hora de la pastilla");
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("TOMAR DISP 3 ");
+    lcd.setCursor(0, 1);
+    lcd.print("  Quetiapina  ");
+
+    setColor();
+
+    for (int i = 0; i < NUMPIXELS; i++) {
+      luz3.setPixelColor(i, luz3.Color(redColor, greenColor, blueColor));
+      luz3.show();
+      delay(200);
+    }
+
+    // Starting the motor that opens the lid
+    servo_4.write(180);
+    delay(1000);
+    servo_4.write(0);
+    delay(1000);
+
+    // reproducir sonido
+    tone(8, 440);
+    delay(500);
+    noTone(8);
+    delay(100);
+
+    // apagar luces servo y limpiar pantalla
+    lcd.clear();
+    contador++;
+    segundo++;
+    if (contador == 3) {
+      horaAlarma2++;
+      for (int i = 0; i < NUMPIXELS; i++) {
+        luz3.setPixelColor(i, 0, 0, 0);
+        luz3.show();
+        delay(200);
+        contador = 0;
+      }
+    }
+  }
+
+  //Alarma 3
+  while (hora == horaAlarma3 && minuto == minutoAlarma3) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Hora de pastilla");
+    delay(500);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("DISPENSADOR");
+    lcd.setCursor(0, 1);
+    lcd.print("1, 2, 3, 4");
+    delay(500);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Clarityne Plus");
+    lcd.setCursor(0, 1);
+    lcd.print("Midazolam");
+    delay(500);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Quetiapina");
+    lcd.setCursor(0, 1);
+    lcd.print("Misoprostol");
+    delay(500);
+    lcd.clear();
+
+    setColor();
+
+    for (int i = 0; i < NUMPIXELS; i++) {
+      luz1.setPixelColor(i, luz1.Color(redColor, greenColor, blueColor));
+      luz4.setPixelColor(i, luz4.Color(redColor, greenColor, blueColor));
+      luz2.setPixelColor(i, luz2.Color(redColor, greenColor, blueColor));
+      luz3.setPixelColor(i, luz3.Color(redColor, greenColor, blueColor));
+      luz1.show();
+      luz2.show();
+      luz3.show();
+      luz4.show();
+      delay(200);
+    }
+
+    // Starting the motor that opens the lid
+    servo_2.write(180);
+    servo_5.write(180);
+    servo_3.write(180);
+    servo_4.write(180);
+    delay(1000);
+    servo_2.write(0);
+    servo_5.write(0);
+    servo_3.write(0);
+    servo_4.write(0);
+
+    delay(1000);
+
+    // reproducir sonido
+    tone(8, 440);
+    delay(500);
+    noTone(8);
+    delay(100);
+
+    // apagar luces servo y limpiar pantalla
+    lcd.clear();
+    contador++;
+    segundo++;
+    if (contador == 4) {
+      horaAlarma3++;
+      for (int i = 0; i < NUMPIXELS; i++) {
+        luz1.setPixelColor(i, 0, 0, 0);
+        luz2.setPixelColor(i, 0, 0, 0);
+        luz3.setPixelColor(i, 0, 0, 0);
+        luz4.setPixelColor(i, 0, 0, 0);
+        luz1.show();
+        luz2.show();
+        luz3.show();
+        luz4.show();
+        contador = 0;
+      }
+    }
+  }
 
   desplegarHora();
   //Inicialmente esta en LOW
   estado1 = digitalRead(PIN_BOTON1);
-  Serial.println(estado1);  
   if (estado1 == HIGH) {  //Esta presionado
     lcd.clear();
     modoConfig();
   }
-  activarDesactivarAlarma();
   resetearVariablesDeControl();
 }
 
@@ -126,9 +315,9 @@ void desplegarHora() {
   if (am_pm == 0) lcd.print(" AM");
   if (am_pm == 1) lcd.print(" PM");
 
-  delay(100);
+  delay(1000);
 
-   segundo = segundo + 1;  //Incremento del contador de segundos
+  segundo = segundo + 1;  //Incremento del contador de segundos
   // Cálculo del reloj con display
   // Gestión de segundos, minutos y horas
   if (segundo == 60) {
@@ -152,17 +341,22 @@ void modoConfig() {
   while (!esConfigHora) {
     configurarHora();
   }
-
   delay(250);
   while (!esAlarma) {
     configurarAlarma();
   }
   delay(250);
+  while (!esAlarma2) {
+    configurarAlarma2();
+  }
+  delay(250);
+  while (!esAlarma3) {
+    configurarAlarma3();
+  }
 }
 
 void configurarHora() {
-  Serial.println("Estoy en config hora");
-  // Ajuste hora  
+  // Ajuste hora
   delay(300);
   lcd.setCursor(0, 0);
   lcd.print("Config hora");
@@ -170,7 +364,7 @@ void configurarHora() {
   if (hora < 10) lcd.print("0");
   lcd.print(hora);
   lcd.print(":");
-  if (minuto < 10) lcd.print("0"); 
+  if (minuto < 10) lcd.print("0");
   lcd.print(minuto);
   lcd.print(":");
   if (segundo < 10) lcd.print("0");
@@ -180,7 +374,7 @@ void configurarHora() {
   if (am_pm == 1) lcd.print(" PM");
 
   estado1 = digitalRead(PIN_BOTON2);
-  if (estado1 == 1)hora = hora + 1;  //incrementa hora 
+  if (estado1 == 1) hora = hora + 1;  //incrementa hora
 
   estado2 = digitalRead(PIN_BOTON3);
   if (estado2 == 1) {
@@ -198,7 +392,6 @@ void configurarHora() {
   if (hora == 13) {
     hora = 1;
     am_pm = am_pm + 1;
-
     if (am_pm == 2) am_pm = 0;
   }
 
@@ -210,11 +403,10 @@ void configurarHora() {
 }
 
 void configurarAlarma() {
-  Serial.println("Estoy en config alarma");
   delay(300);
   lcd.setCursor(0, 0);
   lcd.print("Config alarma");
-  lcd.setCursor(0,1);
+  lcd.setCursor(0, 1);
   if (horaAlarma < 10) lcd.print("0");  // Siempre dos dígitos
   lcd.print(horaAlarma);
   lcd.print(":");
@@ -223,7 +415,6 @@ void configurarAlarma() {
   lcd.print(":");
   if (segundoAlarma < 10) lcd.print("0");
   lcd.print(segundoAlarma);
-
   if (am_pm_alarma == 0) lcd.print(" AM");
   if (am_pm_alarma == 1) lcd.print(" PM");
 
@@ -245,113 +436,105 @@ void configurarAlarma() {
 
     if (am_pm_alarma == 2) am_pm_alarma = 0;
   }
-  estado3 = digitalRead(PIN_BOTON1);//Memeoriza alarma y sale de SETUP alarma
+  estado3 = digitalRead(PIN_BOTON1);  //Memeoriza alarma y sale de SETUP alarma
   if (estado3 == HIGH) {
     esAlarma = true;
     lcd.clear();
   }
 }
 
-void activarDesactivarAlarma() {
-  if (horaAlarma == hora && minutoAlarma == minuto && am_pm_alarma == am_pm && sonarAlarma) {
-    Serial.println("Suena alarma");
-    
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Hora de pastilla");
-    lcd.setCursor(0, 1);
-    lcd.print("Tomar Propanol");
+void configurarAlarma2() {
+  delay(300);
+  lcd.setCursor(0, 0);
+  lcd.print("Config alarma 2");
+  lcd.setCursor(0, 1);
+  if (horaAlarma2 < 10) lcd.print("0");  // Siempre dos dígitos
+  lcd.print(horaAlarma2);
+  lcd.print(":");
+  if (minutoAlarma2 < 10) lcd.print("0");
+  lcd.print(minutoAlarma2);
+  lcd.print(":");
+  if (segundoAlarma2 < 10) lcd.print("0");
+  lcd.print(segundoAlarma2);
+  if (am_pm_alarma2 == 0) lcd.print(" AM");
+  if (am_pm_alarma2 == 1) lcd.print(" PM");
 
-    set_color();
+  estado1 = digitalRead(PIN_BOTON2);
+  if (estado1 == 1) horaAlarma2 = horaAlarma2 + 1;  //Incrementa hora
 
-    for (int i = 0; i < NUMPIXELS; i++) {
-      luz_1.setPixelColor(i, luz_1.Color(red_color, green_color, blue_color));
-      luz_1.show();
-      delay(50);
-    }
-
-    //activar el buzzer
-    tone(PIN_BUZZ, sonido, 1);
-    delay(100);
-    tone(PIN_BUZZ, sonido, 0);
-
-    estado2 = digitalRead(PIN_BOTON3);//Determina si se debe apagar la alarma
-    if (estado2 == HIGH) {
-      Serial.println("Se apaga alarma");
-      noTone(PIN_BUZZ);
-      delay(250);
-      sonarAlarma = false;
-    }
-  } 
-  else {
-    Serial.println("Se apaga alarma segunda vez");
-    Serial.print("Valor de sonarAlarma ");
-    Serial.println(sonarAlarma);
-    noTone(PIN_BUZZ);  //Apaga buzzer
+  estado2 = digitalRead(PIN_BOTON3);
+  if (estado2 == 1) {
+    segundoAlarma2 = 0;
+    minutoAlarma2 = minutoAlarma2 + 1;  //Incrementa minutos
   }
+  if (minutoAlarma2 == 60) {
+    minutoAlarma2 = 0;
+    horaAlarma2 = horaAlarma2 + 1;
+  }
+  if (horaAlarma2 == 13) {
+    horaAlarma2 = 1;
+    am_pm_alarma2 = am_pm_alarma2 + 1;
 
-  if (minutoAlarma == minuto) {
-
-  } else {
-    Serial.println("Se cambia variable a true");
-    sonarAlarma = true;  //Variable a 1 después del minuto de sonido
-  }                 // 1 minuto de alarma
+    if (am_pm_alarma2 == 2) am_pm_alarma2 = 0;
+  }
+  estado3 = digitalRead(PIN_BOTON1);  //Memeoriza alarma y sale de SETUP alarma
+  if (estado3 == HIGH) {
+    esAlarma2 = true;
+    lcd.clear();
+  }
 }
 
-void resetearVariablesDeControl(){
+void configurarAlarma3() {
+  delay(300);
+  lcd.setCursor(0, 0);
+  lcd.print("Config alarma 3");
+  lcd.setCursor(0, 1);
+  if (horaAlarma3 < 10) lcd.print("0");  // Siempre dos dígitos
+  lcd.print(horaAlarma3);
+  lcd.print(":");
+  if (minutoAlarma3 < 10) lcd.print("0");
+  lcd.print(minutoAlarma3);
+  lcd.print(":");
+  if (segundoAlarma3 < 10) lcd.print("0");
+  lcd.print(segundoAlarma3);
+  if (am_pm_alarma3 == 0) lcd.print(" AM");
+  if (am_pm_alarma3 == 1) lcd.print(" PM");
+
+  estado1 = digitalRead(PIN_BOTON2);
+  if (estado1 == 1) horaAlarma3 = horaAlarma3 + 1;  //Incrementa hora
+
+  estado2 = digitalRead(PIN_BOTON3);
+  if (estado2 == 1) {
+    segundoAlarma3 = 0;
+    minutoAlarma3 = minutoAlarma3 + 1;  //Incrementa minutos
+  }
+  if (minutoAlarma3 == 60) {
+    minutoAlarma3 = 0;
+    horaAlarma3 = horaAlarma3 + 1;
+  }
+  if (horaAlarma3 == 13) {
+    horaAlarma3 = 1;
+    am_pm_alarma3 = am_pm_alarma3 + 1;
+
+    if (am_pm_alarma3 == 2) am_pm_alarma3 = 0;
+  }
+  estado3 = digitalRead(PIN_BOTON1);  //Memeoriza alarma y sale de SETUP alarma
+  if (estado3 == HIGH) {
+    esAlarma3 = true;
+    lcd.clear();
+  }
+}
+
+void resetearVariablesDeControl() {
   esConfigHora = false;
   esAlarma = false;
+  esAlarma2 = false;
+  esAlarma3 = false;
 }
 
 // Establecer color aleatorio para luces
-void set_color() {
-  red_color = random(0, 255);
-  green_color = random(0, 255);
-  blue_color = random(0, 255);
-}
-
-// Activar alarma y mover servomotores
-void activate_alarm() {
-  servo_4.write(180);
-  servo_5.write(180);
-  delay(MILLISECONDS_IN_SECOND);
-  servo_4.write(0);
-  servo_5.write(0);
-  delay(MILLISECONDS_IN_SECOND);
-
-  // reproducir sonido
-  tone(8, 440);
-  delay(500);
-  noTone(8);
-  delay(100);
-
-  counter++;
-
-  // apagar luces servo y limpiar pantalla
-  lcd.clear();
-
-  if (counter >= 3) {
-    alarm_hour_1++;
-  }
-}
-
-void guardarAlarma1(String nombreAlarma, int hora, int minuto){
-  //Para guardar cambios en el EEPROM
-  //EEPROM.write(0, variable);
-  //EEPROM.write(1, variable);
-  //EEPROM.write(2, variable);
-}
-
-void guardarAlarma2(String nombreAlarma, int hora, int minuto){
-  //Para guardar cambios en el EEPROM
-  //EEPROM.write(3, variable);
-  //EEPROM.write(4, variable);
-  //EEPROM.write(5, variable);
-}
-
-void guardarAlarma3(String nombreAlarma, int hora, int minuto){
-  //Para guardar cambios en el EEPROM
-  //EEPROM.write(6, variable);
-  //EEPROM.write(7, variable);
-  //EEPROM.write(8, variable);
+void setColor() {
+  redColor = random(0, 255);
+  greenColor = random(0, 255);
+  blueColor = random(0, 255);
 }
